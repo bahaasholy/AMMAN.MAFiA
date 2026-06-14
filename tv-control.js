d5d8462a049f1d0fe7dde33c0fb6e95e054ca72e
bahaasholy <bahaa.sholy@gmail.com>
2026-06-14 20:03:53 +0300

    Add files via upload

--- tv-control.js
+++ tv-control.js
@@ -837,6 +837,7 @@
 
   function getDisplayUrl() {
     const url = new URL('tv.html', window.location.href);
+    url.searchParams.set('v', '46');
     url.searchParams.set('room', roomCode);
     return url.toString();
   }
